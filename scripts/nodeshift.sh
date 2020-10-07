abfc6287c9cf69d587c0052820f9cb4c22c2e2b2
Doug Donahue <ddonahue@redhat.com>
2020-10-07 16:27:28 -0400

    Updated deployment script

--- scripts/nodeshift.sh
+++ scripts/nodeshift.sh
@@ -5,11 +5,12 @@ source ${DIR}/common/logger.sh
 source ${DIR}/common/util.sh
 
 usage() {
-  log-info "Usage: $(basename $0) [deploy|undeploy] <app_name>"
+  log-info "Usage: $(basename $0) [deploy|undeploy] <service_name>"
   exit 1
 }
 
-APP_NAME=${APP_NAME:-mfe-poc}
+NODESHIFT_CMD=`pwd`/node_modules/.bin/nodeshift
+APP_NAME=${APP_NAME:-ums-poc}
 
 NAMESPACE=${NAMESPACE}
 if [ -z "${NAMESPACE}" ]; then
@@ -19,14 +20,16 @@ fi
 
 KSVC_NAME=$2
 if [ -z "${KSVC_NAME}" ]; then
-  log-info "You must specify and app name!"
+  log-info "You must specify service name!"
   usage
 fi
 
 deploy() {
   log-info "nodeshift --knative=true --namespace.name=${NAMESPACE}"
-  nodeshift --knative=true --namespace.name=${NAMESPACE}
+  ${NODESHIFT_CMD} --knative=true --namespace.name=${NAMESPACE}
   dd-oc label ksvc/${KSVC_NAME} app.kubernetes.io/part-of=${APP_NAME} --overwrite=true
+
+  kn service update ${KSVC_NAME} --min-scale 0
 
   local _latest_rev=$(oc --namespace ${NAMESPACE} get ksvc/${KSVC_NAME} -o=jsonpath='{.status.latestCreatedRevisionName}')
   dd-oc label rev/${_latest_rev} app.openshift.io/runtime=nodejs --overwrite=true
