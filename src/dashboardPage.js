04d8f432299d951d3c9caabd64f006f403b0cda2
Matt Nolting <matthew.nolting@gmail.com>
2020-10-06 20:12:12 -0400

    updated dashboard, routes, webpack.config

---
+++ src/dashboardPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { PageSection, PageSectionVariants, Button } from '@patternfly/react-core';
+
+export const DashboardPage = () => {
+  return (
+    <PageSection variant={PageSectionVariants.light}>
+      Hello
+    </PageSection>
+  );
+}
